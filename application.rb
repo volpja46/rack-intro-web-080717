dc2a51846f07082077d1b08eeb7ec3ca4ce0ec84
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-08-28 09:57:24 -0400

    good

--- application.rb
+++ application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World."
+    resp.write "   Hello, my name is Jennnn!"
     resp.finish
   end
 
 end
-
